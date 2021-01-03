9730103e0104710ea1f274ed65b0ec5b3a31b63f
Sagar Adval <sagaradval@gmail.com>
2021-01-03 21:16:47 +0530

    Added maximum sum increasing subsequence

---
+++ Dynamic Programming/MaximumSumIncreasingSubsequence.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+
+
+public:
+    int maxSumIS(int arr[], int n)
+    {
+        int dp[n];
+        for(int i = 0;i<n;i++) dp[i] = arr[i];
+
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    if(dp[i] < dp[j] + arr[i])
+                    {
+                        dp[i] = dp[j] + arr[i];
+                    }
+                }
+            }
+            // for(auto x:dp) cout<<x<<" ";
+            // cout<<endl;
+            // cout<<endl;
+        }
+        return *max_element(dp, dp+n);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+
+
+
+        Solution ob;
+        cout << ob.maxSumIS(a, n) << "\n";
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
